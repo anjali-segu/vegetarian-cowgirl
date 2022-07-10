9f56cd4664eb9ae58a77fc144c0c7c87d16fff78
pulchroxloom <code@dantebarbieri.dev>
2022-07-10 13:00:50 -0500

    Update under construction page

--- src/organism/UnderConstruction/index.tsx
+++ src/organism/UnderConstruction/index.tsx
@@ -3,7 +3,8 @@ import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import ThemeProvider from '@mui/styles/ThemeProvider'
 import Typography from '@mui/material/Typography'
-import UnderConstructionIcon from '../../assets/images/icons/other_icon.png'
+import SiteLogo from '../../assets/images/icons/logos/LogoBlack.png'
+import UnderConstructionIcon from '../../assets/images/icons/construction_icon.png'
 import Header from '../../atom/Header'
 import {theme} from '../../utils/theme'
 import Box from '@mui/material/Box'
@@ -15,20 +16,23 @@ const UnderConstruction = (props: Props) => {
     <ThemeProvider theme={theme}>
         <Grid container justifyContent='center' sx={{marginTop:theme.spacing(2)}}>
          <Grid item xs={1} sx={{display:'flex',justifyContent:'center'}}>
-          <img style={{maxHeight: theme.spacing(13)}} src={UnderConstructionIcon} alt="Paris"/>
+          <img style={{maxHeight: theme.spacing(13)}} src={SiteLogo} />
          </Grid>
        </Grid>
       <Header  text={"Under Construction"} margin={1}/>
       <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <Typography variant='h5' sx=
+        <Typography variant='h4' sx=
         {{
-            padding:theme.spacing(8),
+            fontSize:theme.spacing(7),
+            padding:theme.spacing(12),
 
         }}>Check back soon! </Typography>
+        <img style={{maxHeight: theme.spacing(80)}} src={UnderConstructionIcon} />
         <Button href='/' sx={{
-        marginTop: theme.spacing(4),
+        margin: theme.spacing(15),
         backgroundColor: "#f09b85",
         borderRadius: 0,
+        fontSize:theme.spacing(4),
         "&:hover": {
           backgroundColor: "#ffffff",
           color: "#f09b85",
